fefed322441e751af95966c5b55eebbe98312c37
Jan Klaas Kollhof <jan@kollhof.net>
2018-10-31 20:50:32 +1100

    moved code to example index.js

--- examples/index.js
+++ examples/index.js
@@ -1,30 +1,53 @@
 /* global document */
 import leaflet from 'leaflet';
-import {render} from './utils';
+import {getCRSByHeading} from 'leaflet-nearmap/helper/getCRSByHeading';
+import {getLayerByHeading} from 'leaflet-nearmap/helper/getLayerByHeading';
 
 
 /**
  * configuration
  */
 const demoKey ='Yzc2MjEzMWUtY2Q4YS00NTM2LTgyMDgtMDljZjI2YTdhMTMz';
-const url = `https://api.nearmap.com/tiles/v3/{contentType}/{z}/{x}/{y}.img?tertiary=default&apikey=${demoKey}`;
+const urlTemplate = `https://api.nearmap.com/tiles/v3/{contentType}/{z}/{x}/{y}.img?tertiary=default&apikey=${demoKey}`;
 
 
-function initMap() {
-  // demo area that is available for demo API-key
-  const lat = -34.915302;
-  const lng = 138.595637;
-  const zoom = 13;
-  const heading = 'HEADING_NORTH';
-
-  const map = leaflet.map('mapid', {
-    center: [lat, lng],
-    zoom
+/**
+*  setup a popup with coords where you clicked on
+**/
+function initPopup(map) {
+  map.on('click', (coords)=> {
+    leaflet.popup()
+      .setLatLng(coords.latlng)
+      .setContent(`You clicked the map at ${coords.latlng.toString()}`)
+      .openOn(map);
   });
+}
 
-  render(map, {url, heading});
+/**
+ *  Update process
+ *  1. Remove existing TileLayer
+ *  2. Generate new CRS
+ *  3. Inject CRS into Map Oject
+ *  4. Generate layer with new heading
+ *  5. Add new layer
+ */
+export function updateMap(map, url, heading) {
+  // Get current zoom level and coords for next view
+  const zoom = map.getZoom();
+  const center = map.getCenter();
 
-  return map;
+  // Remove existing TileLayer
+  map.eachLayer((existingLayer)=> map.removeLayer(existingLayer));
+
+  // Inject crs into Map Oject
+  map.options.crs = getCRSByHeading(heading);
+  // reset to previous center and zoom
+  map.setView(center, zoom);
+
+  // Generate layer with heading
+  const layer = getLayerByHeading(heading, url);
+  // Add new layer to map
+  map.addLayer(layer);
 }
 
 
@@ -36,14 +59,28 @@ function initButtons(map) {
   for (const id of buttons) {
     document.getElementById(id).addEventListener('click', ({target})=> {
       const heading = target.getAttribute('data-heading');
-      render(map, {url, heading});
+      updateMap(map, urlTemplate, heading);
     });
   }
 }
 
 
 function init() {
-  const map = initMap();
+  // demo area that is available for demo API-key
+  const lat = -34.915302;
+  const lng = 138.595637;
+  const zoom = 13;
+  const heading = 'HEADING_NORTH';
+
+  const map = leaflet.map('mapid', {
+    center: [lat, lng],
+    zoom
+  });
+
+  updateMap(map, urlTemplate, heading);
+
+  initPopup(map);
+
   initButtons(map);
 }
 
